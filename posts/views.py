9d6e3478de6870cfa2f5387dc708fa1f8624601c
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-07-09 01:16:42 +0300

    second commit

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
 from .models import Post
 
 # Create your views here.
@@ -11,3 +14,60 @@ def homepage(request):
 def post(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, 'post.html', {'post':post})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+
+        if password == password2:                      
+            if User.objects.filter(username = username).exists():
+                messages.info(request, 'Username Already exist')
+                return redirect('register')
+            
+            elif User.objects.filter(email = email).exists():
+                messages.info(request, 'Email Already exist')
+                return redirect('register')
+            
+            else:
+                user= User.objects.create_user(username = username, email = email, password = password)
+                user.save()
+                return redirect('login')
+            
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+        
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+    
+        else:
+            messages.info(request, 'Invalid Credintial')
+            return redirect('login')
+        
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
